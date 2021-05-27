0703621b4174f27dc500b90164bd56354988a766
literat <tomas@litera.me>
2021-05-27 10:23:57 +0200

    feat: add setup for code coverage

--- config/jest/config.js
+++ config/jest/config.js
@@ -3,4 +3,5 @@ module.exports = {
   setupFiles: ['./config/jest/setup.js'],
   testEnvironment: 'jsdom',
   snapshotSerializers: ['enzyme-to-json/serializer'],
+  reporters: ['default', ['jest-junit', { outputDirectory: './coverage' }]],
 };
